f16085fdf1012fb9e271b2a4af10bb7f43e77b6f
Ferreira-Orlann <ferreira.orlann@gmail.com>
2024-10-26 16:41:25 +0200

    Doc and Ticket

--- src/routers/ticket.js
+++ src/routers/ticket.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
 import { z } from "zod"
-import { ValidateQueryString } from "../middlewares/datavalidation";
+import { BuildErrorJson } from "../factories/error.js";
+import { ErrorTypes } from "../enums/errortypes.js";
 
 export const TicketRouter = Router()
 
-TrainRouter.param("uuid", )
-TicketRouter.get("/:uuid", (req, res, next, value) => {
-    
+const uuidSchema = z.string().uuid();
+
+TicketRouter.param("uuid", (req, res, next, value) => {
+    uuidSchema.parseAsync(value).then((result) => {
+        next()
+    }).catch((err) => {
+        res.status(422).json(BuildErrorJson(ErrorTypes.DATA_VALIDATION, "Invalid UUID, need UUID v4"))
+    })    
+})
+TicketRouter.get("/:uuid", (req, res) => {
+    res.sendStatus(200)
 })
